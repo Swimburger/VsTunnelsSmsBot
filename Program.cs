c2f7592e3a16aee5ca81ba6a72e8d48ef1c9aff4
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2022-11-02 17:42:06 -0400

    Configure Voice URL with VS Tunnel URL automatically

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
 using Twilio.AspNet.Core.MinimalApi;
 using Twilio.TwiML;
+using Twilio.AspNet.Core;
+using Twilio.Clients;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// add TwilioRestClient to Dependency Injection Container
+// configured using 'Twilio:Client:AccountSid' and 'Twilio.Client:AuthToken'
+builder.Services.AddTwilioClient();
+
 var app = builder.Build();
 
 app.MapPost("/message", () =>
@@ -11,4 +20,31 @@ app.MapPost("/message", () =>
     return Results.Extensions.TwiML(messagingResponse);
 });
 
-app.Run();
+await app.StartAsync();
+
+var devTunnelUrl = Environment.GetEnvironmentVariable("VS_TUNNEL_URL");
+if (devTunnelUrl != null)
+{
+    using var serviceScope = app.Services.CreateScope();
+    var twilioClient = serviceScope.ServiceProvider.GetRequiredService<ITwilioRestClient>();
+    var phoneNumber = app.Configuration["Twilio:PhoneNumber"];
+    if (string.IsNullOrEmpty(phoneNumber)) throw new Exception("'Twilio:PhoneNumber' not configured");
+
+    var phoneNumberResources = await IncomingPhoneNumberResource.ReadAsync(
+        phoneNumber: new PhoneNumber(phoneNumber),
+        client: twilioClient
+    );
+    var phoneNumberResource = phoneNumberResources.First();
+
+    var smsUrl = $"{devTunnelUrl}message";
+    await IncomingPhoneNumberResource.UpdateAsync(
+        pathSid: phoneNumberResource.Sid,
+        smsUrl: new Uri(smsUrl),
+        smsMethod: Twilio.Http.HttpMethod.Post,
+        client: twilioClient
+    );
+
+    app.Logger.LogInformation("Updated {TwilioPhoneNumber} SMS URL to {SmsUrl}", phoneNumber, smsUrl);
+}
+
+await app.WaitForShutdownAsync();
